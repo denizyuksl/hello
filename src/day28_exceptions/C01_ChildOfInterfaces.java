a9b015ca6b8765fa15b70af87331ddbfa2fbfc33
denizyuksl <yukseldnz.33@gmail.com>
2023-10-17 20:48:58 +0300

    hello

---
+++ src/day28_exceptions/C01_ChildOfInterfaces.java
@@ -0,0 +1,43 @@
+package day28_exceptions;
+
+import day27_abstractClasses_interface.I08_Interface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C01_ChildOfInterfaces implements I01_InterfacesIstisnaiDurum {
+
+
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public int method2() {
+        return 0;
+    }
+
+    @Override
+    public String method3() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(I01_InterfacesIstisnaiDurum.methodIstisna2()); // 44
+
+        C01_ChildOfInterfaces obj = new C01_ChildOfInterfaces();
+        obj.runnerMethod();
+
+
+        List<String> liste = new ArrayList<>();
+
+    }
+
+
+    public void runnerMethod() {
+        methodIstisna(); // default istisnai method
+    }
+
+}
